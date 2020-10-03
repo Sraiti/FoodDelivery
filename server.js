52d64b815be460f03f2d78af4438373606e6185e
Sraiti <abdellsraiti019@gmail.com>
2020-10-03 06:13:41 +0100

    view added login/register

--- server.js
+++ server.js
@@ -1,29 +1,33 @@
-if(process.env.NODE_ENV!=='production'){
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const expressLayout =require('express-ejs-layouts');
-const port = 3000||process.env.PORT;
-const indexRouter =require('./routes/index');
+ const expressLayout = require('express-ejs-layouts');
+const port = 3000 || process.env.PORT;
+const indexRouter = require('./routes/index');
+const registerRouter = require('./routes/register');
+const loginRouter = require('./routes/login');
 
 
-app.set('view engine','ejs');
-app.set('views',__dirname+'/views');
-app.set('layouts','layouts/layout');
-
-app.use('/', indexRouter);
+app.set('views', __dirname + '/views');
 app.use(expressLayout);
+app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use('/', indexRouter);
+app.use('/', registerRouter);
+app.use('/', loginRouter);
 
-mongoose.connect(process.env.DATABASE_URL, 
-    {useNewUrlParser: true, useUnifiedTopology: true});
-const db =mongoose.connection;
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("MongDB connected");
+db.once('open', function () {
+    console.log("MongDB connected");
 });
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port port!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
